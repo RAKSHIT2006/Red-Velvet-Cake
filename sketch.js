2bec75d750611a7b876e217349d4297de6c48bef
RAKSHIT2006 <72135785+RAKSHIT2006@users.noreply.github.com>
2020-11-10 19:24:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+Needed ItemList;
+
+cake flour(3 cup)(spoon & leveled).
+baking soda (1 teaspoon).
+unsweetened natural cocoa powder (2 tablespoon/10g).
+salt(1/2 teaspoon).
+unsalted butter (1/2 cup / 115g).
+granulated sugar (400g).
+vinegar. 
+vegetable Oil . 
+butter .
+milk.
+
+
+CAKE'S Process;
+ STEP 1 ;
+      take the milk in a bowl and add 1 teaspoon vinegar and mix that,add oil and sugar mix it. 
+
+ STEP 2 ;
+       after mixing it take cake flour you needed, put cocoa and mix this and backingsoda mix this nicely,salt 1/2 spoon
+
+ STEP 3 ;      
+       add vanilla essance and red food colour add hot water and mix it. 
+   
+ STEP 4 ;
+        put it in a cake bowl and fix it into Oven in 30 min.
+
+ STEP 5 ;    
+        whipping cream 1/2 cup and add sugar mix it while it thick;
+
+ STEP 6 ;        
+       
+
+            
+
+
+
